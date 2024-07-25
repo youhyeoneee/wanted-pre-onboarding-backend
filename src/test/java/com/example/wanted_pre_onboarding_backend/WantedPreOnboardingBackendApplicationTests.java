6355dc61f7353788324bac4551ecbb2b47959a3f
youhyeoneee <khukyh2018@khu.ac.kr>
2024-07-25 17:59:25 +0900

    first commit

---
+++ src/test/java/com/example/wanted_pre_onboarding_backend/WantedPreOnboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wanted_pre_onboarding_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreOnboardingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
