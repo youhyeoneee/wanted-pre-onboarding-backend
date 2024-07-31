7d6d257f0cdb453d1ebf03b7d1873b31d0829357
youhyeoneee <khukyh2018@khu.ac.kr>
2024-07-31 17:38:01 +0900

    test: CompanyRepository 단위 테스트 추가
    - 회사 아이디 존재 여부 확인 테스트 (성공, 실패)
    - 테스트용 인메모리 데이터베이스 설정
    
    #1

--- src/main/java/com/example/wanted_pre_onboarding_backend/domain/company/Company.java
+++ src/main/java/com/example/wanted_pre_onboarding_backend/domain/company/Company.java
@@ -3,11 +3,13 @@ package com.example.wanted_pre_onboarding_backend.domain.company;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "companys")
 public class Company {
     @Id
