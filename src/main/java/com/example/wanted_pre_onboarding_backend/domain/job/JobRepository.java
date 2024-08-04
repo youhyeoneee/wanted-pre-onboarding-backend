f2a211eb386b1add25eca777f3c6f38654ab03a3
youhyeoneee <khukyh2018@khu.ac.kr>
2024-08-04 23:05:16 +0900

    feat: 채용공고 상세 조회 API 구현
    - JobService - findJobById, findJobsByCompanyId, getOtherJobIds 메서드 구현
    - JobController - getJobDetail 메서드 구현
    - JobDetailResponseDto 추가
    
    #9

--- src/main/java/com/example/wanted_pre_onboarding_backend/domain/job/JobRepository.java
+++ src/main/java/com/example/wanted_pre_onboarding_backend/domain/job/JobRepository.java
@@ -1,7 +1,10 @@
 package com.example.wanted_pre_onboarding_backend.domain.job;
 
+import java.util.List;
+
 import com.example.wanted_pre_onboarding_backend.domain.job.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobRepository extends JpaRepository<Job, Integer> {
+	List<Job> findByCompanyId(Integer companyId);
 }
