b92228189ace6abc7f258fe9342c1ff4e0ef234c
youhyeoneee <khukyh2018@khu.ac.kr>
2024-08-05 16:10:39 +0900

    feat: 유저 엔티티 테이블 연결 및 레포지토리 추가
    #10

---
+++ src/main/java/com/example/wanted_pre_onboarding_backend/domain/user/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.wanted_pre_onboarding_backend.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
