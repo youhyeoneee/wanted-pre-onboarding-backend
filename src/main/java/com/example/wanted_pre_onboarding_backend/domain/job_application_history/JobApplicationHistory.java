33b9aa7cda6bcb58a2fb3a660fb0a306720fe532
youhyeoneee <khukyh2018@khu.ac.kr>
2024-08-05 16:10:10 +0900

    feat: 채용공고 지원 내역 엔티티 및 레포지토리 추가
    #10

---
+++ src/main/java/com/example/wanted_pre_onboarding_backend/domain/job_application_history/JobApplicationHistory.java
@@ -0,0 +1,50 @@
+package com.example.wanted_pre_onboarding_backend.domain.job_application_history;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.example.wanted_pre_onboarding_backend.domain.company.Company;
+import com.example.wanted_pre_onboarding_backend.domain.job.Job;
+import com.example.wanted_pre_onboarding_backend.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "job_application_histories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class JobApplicationHistory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "job_id")
+	private Job job;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@CreationTimestamp
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime createdAt;
+
+	public JobApplicationHistory(Job job, User user) {
+		this.job = job;
+		this.user = user;
+	}
+}
